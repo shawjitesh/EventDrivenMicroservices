eab7ed7e7e02f2f47ed163740a145179ace910ee
shawjitesh <jiteshshaw93@gmail.com>
2025-05-06 00:41:13 +0530

    added xToKafkaService

---
+++ x-to-kafka-service/src/main/java/com/microservices/poc/x/to/kafka/service/XToKafkaServiceApplication.java
@@ -0,0 +1,11 @@
+package com.microservices.poc.x.to.kafka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XToKafkaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XToKafkaServiceApplication.class, args);
+    }
+}
---
+++ x-to-kafka-service/src/test/java/com/microservices/poc/x/to/kafka/service/XToKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.poc.x.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class XToKafkaServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        // This method is intentionally left empty. The purpose of this test is to check if the application context loads successfully.
+    }
+}
